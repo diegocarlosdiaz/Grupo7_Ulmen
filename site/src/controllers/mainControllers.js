d0072b32d5561c51e6f6e0a79036e5b93c94c82c
diegocarlosdiaz <diegocarlosdiaz5@gmail.com>
2022-01-30 23:38:05 -0300

    arreglos en home y partials

--- site/src/controllers/mainControllers.js
+++ site/src/controllers/mainControllers.js
@@ -1,4 +1,6 @@
 const db = require('../database/models');
+const sequelize = db.sequelize;
+const { Op } = require("sequelize");
 
 const controllers = {
     home: (req, res) => { 
@@ -17,6 +19,35 @@ const controllers = {
     },
     nosotros: (req, res) => {
         return res.render('nosotros') 
+    },
+    search: (req, res) => {
+        db.Producto.findAll({
+            include: ['Categoria'],
+            include: { association: 'imagenes' },
+            where: {
+                [Op.or]: [
+                    {
+                        nombre: {
+                            [Op.substring]: req.query.search
+                        }
+                    },
+                    {
+                        descripcion: {
+                            [Op.substring]: req.query.search
+                        }
+                    }
+                ]
+            }
+        })
+            .then(productos => {
+                return res.render('searchResults', {
+                    title: "Resultado de la búsqueda",
+                    productos,
+                    busqueda: req.query.busqueda
+                })
+            })
+            .catch(error => console.log(error))
+
     }
 }
 
--- site/src/routes/main.js
+++ site/src/routes/main.js
@@ -1,8 +1,9 @@
 const router = require('express').Router()
 const path = require('path')
-const {home, nosotros} = require('../controllers/mainControllers')
+const {home, nosotros,search} = require('../controllers/mainControllers')
 
 
 router.get('/', home),
 router.get('/nosotros', nosotros)
+router.get('/search', search)
 module.exports = router
